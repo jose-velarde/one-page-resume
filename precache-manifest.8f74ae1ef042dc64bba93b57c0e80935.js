f4bada36b10df9b83456269b7817cd910232436b
Jose Velarde <josevelarde00@gmail.com>
2021-07-16 15:25:27 -0300

    Updates

---
+++ precache-manifest.8f74ae1ef042dc64bba93b57c0e80935.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef42cd0806c1f5df4f5f4af1bf352c1",
+    "url": "/one-page-resume/index.html"
+  },
+  {
+    "revision": "fdfaf0e5241ed2c5e15f",
+    "url": "/one-page-resume/static/css/2.bc6b4572.chunk.css"
+  },
+  {
+    "revision": "b15ab267508e6a043b26",
+    "url": "/one-page-resume/static/css/main.ca9cfbde.chunk.css"
+  },
+  {
+    "revision": "fdfaf0e5241ed2c5e15f",
+    "url": "/one-page-resume/static/js/2.e6b13723.chunk.js"
+  },
+  {
+    "revision": "d326ad146adc54f60a96478cb811ae70",
+    "url": "/one-page-resume/static/js/2.e6b13723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e2ccc39fc289d56d0c",
+    "url": "/one-page-resume/static/js/3.493e78d6.chunk.js"
+  },
+  {
+    "revision": "b15ab267508e6a043b26",
+    "url": "/one-page-resume/static/js/main.45bacf22.chunk.js"
+  },
+  {
+    "revision": "931831d9415020993a41",
+    "url": "/one-page-resume/static/js/runtime-main.3d53dab6.js"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/one-page-resume/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/one-page-resume/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/one-page-resume/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/one-page-resume/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/one-page-resume/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/one-page-resume/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/one-page-resume/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/one-page-resume/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
